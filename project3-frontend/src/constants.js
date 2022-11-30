319bb22d792434c3d1d59dafbf3c7983d2d63ca1
Hunter Finch <hunter.finch@icloud.com>
2022-11-30 16:38:24 -0600

    updates

--- project3-frontend/src/constants.js
+++ project3-frontend/src/constants.js
@@ -1,4 +1,4 @@
-export const PRODUCTION_API_BASE_URL = "https://csce-315-project-3-1s47.onrender.com/"
+export const PRODUCTION_API_BASE_URL = "https://csce-315-project-3-1s47.onrender.com"
 
 export const DEVELOPMENT_API_BASE_URL = "http://localhost:3001"
 
