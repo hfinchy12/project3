1eafa3f1086ef150497018efd7d6ee83c8008c1b
Hunter Finch <hunter.finch@icloud.com>
2022-12-06 21:22:14 -0600

    new

--- project3-backend/server.js
+++
@@ -1,7 +0,0 @@
-const app = require("./app")
-
-const PORT = process.env.port || 3001
-
-app.listen(PORT, () => {
-  console.log(`🚀 Server listening on http://localhost:${PORT}`)
-})
--- project3-frontend/src/constants.js
+++
@@ -1,5 +0,0 @@
-export const PRODUCTION_API_BASE_URL = "https://csce-315-project-3-1s47.onrender.com"
-
-export const DEVELOPMENT_API_BASE_URL = "http://localhost:3001"
-
-export const API_BASE_URL = PRODUCTION_API_BASE_URL;
